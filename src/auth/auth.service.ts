53d7569c995fedeab4f14eb2a3077c221a755fd6
Rajendra Mandliya <rajendra.mandliya@systango.com>
2025-12-01 20:08:23 +0530

    pass hash and mongo install

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dto/registerUser.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
-  register(registerUserDto: RegisterDto) {
+  async register(registerUserDto: RegisterDto) {
+    await bcrypt.hash(registerUserDto.password, 10);
     return this.userService.createUser(registerUserDto);
   }
 }
